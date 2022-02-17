11783eecc7559fb22cc1b5d50f81e91864b58658
xgaitan <xiomiliz.99@gmail.com>
2022-02-17 09:43:08 -0500

    added auth

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -53,3 +53,8 @@
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
